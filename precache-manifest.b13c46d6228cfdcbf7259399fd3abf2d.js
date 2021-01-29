68ec0c8cefd7524fb60accec903b3c24cb7013a6
parksangsoo <pssw0728@gmail.com>
2021-01-29 15:15:23 +0900

    Updates

---
+++ precache-manifest.b13c46d6228cfdcbf7259399fd3abf2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994a8e9a2834fe6e05ea0b28ce110050",
+    "url": "/youtube2/index.html"
+  },
+  {
+    "revision": "6309a065c080809c5727",
+    "url": "/youtube2/static/css/main.a63c73f0.chunk.css"
+  },
+  {
+    "revision": "bf624fe8ca72dfed662d",
+    "url": "/youtube2/static/js/2.cd42944f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube2/static/js/2.cd42944f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6309a065c080809c5727",
+    "url": "/youtube2/static/js/main.fad51e7c.chunk.js"
+  },
+  {
+    "revision": "38c8cd3113de539ca373",
+    "url": "/youtube2/static/js/runtime-main.0831d75c.js"
+  }
+]);
